f004281bdd218e1511c0de3a89c1ff168d9dd83b
Ken <kabutukenneth3@gmail.com>
2023-06-06 06:14:36 +0300

    testing git

---
+++ src/components/movieDetails/MovieDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MovieDetail() {
+  return (
+    <div>
+      <h1>Hello, Git!</h1>
+      <p>This is a test to check if Git push is working correctly.</p>
+    </div>
+  );
+}
+
+export default MovieDetail;
