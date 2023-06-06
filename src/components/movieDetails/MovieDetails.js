95020e489b71801853d0503066664eaeeb87e3bd
Ken <kabutukenneth3@gmail.com>
2023-06-06 06:21:14 +0300

    initial commit

--- src/components/movieDetails/MovieDetails.js
+++ src/components/movieDetails/MovieDetails.js
@@ -3,8 +3,7 @@ import React from 'react';
 function MovieDetail() {
   return (
     <div>
-      <h1>Hello, Git!</h1>
-      <p>This is a test to check if Git push is working correctly.</p>
+      <h1>Hello, MovieDetail</h1>
     </div>
   );
 }
