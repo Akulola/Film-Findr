48307c1121647f505c430f252cae435e7dded326
Angela <angelakulola4@gmail.com>
2023-06-06 08:45:33 +0300

    Added code for Movie List

--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 
-function MovieList() {
+const  MovieList = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=2801d62ffaec700bd700b8cdb258437b')
+            .then(response => response.json())
+            .then((data) => setMovies(data.results))
+            .catch((error) => console.log('Error fetching movies', error));
+        }, []);
     return (
         <div>
-            <h1> Movie List</h1>
+            <h2> Movie List</h2>
+            {movies.map((movie) => (
+                <div key={movie.id}>
+                    <h3>{movie.title}</h3>
+                    <img 
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                    />
+                    <p>Rating: {movie.vote_average} </p>
+                    <p>{movie.overview}</p>
+                    </div>
+            ))}
         </div>
     );
-}
+};
 
 export default MovieList;
