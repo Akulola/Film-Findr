39aa246b19a5108b7075b9d6c060acc83e627a3a
Ken <kabutukenneth3@gmail.com>
2023-06-06 21:24:02 +0300

    update state

--- src/components/movieDetails/MovieDetails.js
+++ src/components/movieDetails/MovieDetails.js
@@ -8,6 +8,25 @@ function MovieDetail() {
     const [movie, setMovie] = useState(null); 
     const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        //fetch movie detail from API
+        const fetchMovieDetail = async () => {
+            try {
+                const response = await fetch(`/api/movies/${id}`);
+                const data = await response.json();
+                setMovie(data);
+                setLoading(false);
+            } catch (error) {
+                console.log('Error fetching movie detail:', error);
+                setLoading(false);
+            }
+        };
+
+        fetchMovieDetail();
+    }, [id]);
+
+    if ()
+
 
   return (
     <div>
