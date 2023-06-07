91ed8bcc9e219fe1dd50fb0e0ccfb94fe766d5c9
Ruth <osukukuruth@gmail.com>
2023-06-07 18:57:15 -0400

    search-component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,3 +3,6 @@ import Transaction from "./Transaction";
 function MovieSearch() {
     const [searchQuery, setSearchQuery] = useState('');
   const navigate = useNavigate();
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
