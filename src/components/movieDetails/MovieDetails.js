9473de05fe9661c17348a495886928ba12923b7b
Ken <kabutukenneth3@gmail.com>
2023-06-07 20:52:18 +0300

    adding an image tag

--- src/components/movieDetails/MovieDetails.js
+++ src/components/movieDetails/MovieDetails.js
@@ -38,7 +38,7 @@ function MovieDetail() {
   return (
     <div>
       <h2>{movie.title}</h2>
-      <img src={movie.image} alt={movie.title} />
+      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
       
       <h3>User Reviews</h3>
       <ul>
