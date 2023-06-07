21e436f16597729e6534838a3aeaa0587b35c3e6
reaganmunene <reagannmunene@gmail.com>
2023-06-07 17:07:05 +0300

    imported necessary dependancies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
+import React from "react"; //note 
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+    <main>
+      <Routes>
+        <Route path="" element={} />
+        <Route path="" element={} />
+      </Routes>
+    </main>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
