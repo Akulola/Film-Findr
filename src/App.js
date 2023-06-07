131975b0bbe94d275cbd2c228c14a2cfc45b1b6d
Mary <marymukami34@gmail.com>
2023-06-07 18:22:01 +0300

    updating App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import MovieList from './MovieList';
+import MovieDetails from './MovieDetail'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
+      <Header text="Film-Findr" />
+
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+      </Switch>
+
+      <Footer />
     </div>
+    </Router>
   );
-}
+};
 
 export default App;
