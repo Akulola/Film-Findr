07989011ab66995b9d2e639c4c57c141affdd067
reaganmunene <reagannmunene@gmail.com>
2023-06-07 17:33:34 +0300

    imported MovieList component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react"; //note
 import { BrowserRouter , Routes, Route } from "react-router-dom";
 import { Outlet, Link } from "react-router-dom";
 import './App.css';
+import MovieList from './components/movieList'
 
 function App() {
   return (
     <BrowserRouter>
     <main>
       <Routes>
-        <Route path="/" element={} /> //should link to 'home' component
+        <Route path="/" element={MovieList} /> //should link to 'home' component
         <Route path="" element={} />
       </Routes>
     </main>
