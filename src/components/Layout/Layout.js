a91bbc130bdd3fb12c7009f9dbfd104a702c81bc
reaganmunene <reagannmunene@gmail.com>
2023-06-07 10:03:59 +0300

    deleted a link from layout folder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -10,9 +10,6 @@ const Layout = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
