b1ed7d6e26effec4b3fc75f14ef861a4d32f6210
Ruth <osukukuruth@gmail.com>
2023-06-07 18:56:22 -0400

    search-component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,2 +1,5 @@
 import React, { useState } from 'react';
 import Transaction from "./Transaction";
+function MovieSearch() {
+    const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate();
