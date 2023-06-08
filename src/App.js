2950e61b52b124cadfdfb235b85f7c1a406bbcdf
Ruth <osukukuruth@gmail.com>
2023-06-07 21:08:28 -0400

    search-component

--- src/App.js
+++ src/App.js
@@ -3,3 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import MovieSearch from './components/MovieSearch';
+function Home() {
+  return <h1>Home</h1>;
+}
