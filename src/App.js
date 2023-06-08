18d344a47036688c1db402877cab8e7963657911
Ruth <osukukuruth@gmail.com>
2023-06-07 21:07:09 -0400

    search-component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
-
-export default App;
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
