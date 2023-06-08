2cca4cffe0d3c4224851c79fb00ece935e2596c6
AngieKulola <122678471+Akulola@users.noreply.github.com>
2023-06-08 15:36:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react"; //note 
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+// import { NavLink, Outlet, } from "react-router-dom";
+import {  Link } from "react-router-dom";
 import './App.css';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import MovieList from './MovieList';
-import MovieDetails from './MovieDetail'
+import MovieList from './components/movieList'
+import MovieDetail from './components/movieDetails'
+import MovieSearch from './components/search'
 
 function App() {
   return (
-    <Router>
-    <div className="App">
-      <Header text="Film-Findr" />
-
-      <Switch>
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={MovieDetails} />
-      </Switch>
-
-      <Footer />
-    </div>
-    </Router>
+    <BrowserRouter>
+    <header>
+      <nav>
+        <h1>Film Findr</h1>
+        <Link to=""/>
+      </nav>
+    </header>
+    <main >
+      <Routes>
+        <Route path="/" element={MovieList} />  
+        <Route path="/movies/:id" element={MovieDetail} />
+        <Route path="/movie" element={MovieSearch} />
+      </Routes>
+    </main>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
