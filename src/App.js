fce5f0d985943f7a19d0f89fcb311f8d39d9c9a6
Ruth <osukukuruth@gmail.com>
2023-06-08 10:42:33 -0400

    Done Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-// App.js
+// // App.js
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Header from './components/header';
+// import MovieSearch from './components/Search';
+// function Home() {
+//   return <h1>Home</h1>;
+// }
+// function About() {
+//   return <h1>About</h1>;
+// }
+// function App() {
+//   return (
+//     <Router>
+//        <Routes>
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import MovieSearch from './components/Search';
-function Home() {
-  return <h1>Home</h1>;
-}
-function About() {
-  return <h1>About</h1>;
-}
+import Search from './Search';
+
 function App() {
   return (
-    <Router>
-       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Movie Search App</h1>
+      <Search />
+    </div>
   );
 }
 
 export default App;
+
