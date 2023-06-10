6257181395515e817db125ac8e050d03d2aedef7
Ken <kabutukenneth3@gmail.com>
2023-06-10 10:04:19 +0300

    adding info on deader component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header>
-            
+            <div className="logo">
+                
+            </div>
         </header>
     )
 }
