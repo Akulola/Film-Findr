338d50dd990bba4375e441a4933358890c9584bf
Ken <kabutukenneth3@gmail.com>
2023-06-10 10:14:21 +0300

    adding info on deader component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,6 +9,10 @@ function Header() {
             </div>
             <nav>
             <ul className="nav-links">
+                <li>
+                <Link to="/">Home</Link>
+                </li>
+            </ul>
             </nav>
         </header>
     )
