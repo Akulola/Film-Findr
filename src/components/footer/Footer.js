a954a25faec1b4c27cbee7c7bdaa62b7fa89a558
Ken <kabutukenneth3@gmail.com>
2023-06-10 10:22:21 +0300

    adding info on the footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,9 +7,10 @@ function Footer() {
             <div className='footer-content'>
             <p>&copy; 2023 Film Findr. All rights reserved.</p> 
             <nav>
-            <ul className="footer-links">
-            <li><a href="/">Terms of Service</a></li>
-            </ul>
+                <ul className="footer-links">
+                 <li><a href="/">Terms of Service</a></li>
+                 <li><a href="/">Privacy Policy</a></li>
+                </ul>
             </nav>
             </div>
         </footer>
