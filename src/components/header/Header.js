feb9ac5e124368d5411de0bb92d981a2b4960019
Mary <marymukami34@gmail.com>
2023-06-11 22:58:23 +0300

    updating Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter, faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 function Header() {
-
   const logoImage = "https://i.pinimg.com/564x/cb/c8/ae/cbc8aede6fda829e2e0eb6887082f90d.jpg";
 
     return (
@@ -14,7 +11,7 @@ function Header() {
             <Link to="/" className='app-name'>
           <img src={logoImage} alt="Logo" className="logo-image" />
         </Link>
-              <h1 className="header-title" style={{ fontFamily: "Raleway, sans-serif", fontSize: "24px", color: "#333" }}>Film-Findr</h1>
+              <h1 className="header-title" style={{ fontFamily: "Raleway, sans-serif", fontSize: "24px", color: "#fff" }}>Film-Findr</h1>
             </div>
             <nav className='navbar'>
             <ul className="nav-links">
@@ -26,11 +23,7 @@ function Header() {
                 </li>
             </ul>
             </nav>
-             <div className="headerRight">
-              <FontAwesomeIcon icon={faTwitter} className="social-icon"/>
-              <FontAwesomeIcon icon={faInstagram} className="social-icon"/>
-              <FontAwesomeIcon icon={faFacebook} className="social-icon"/>
-      </div>
+             
         </header>
     );
 };
