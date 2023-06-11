fd8ce24b6ff86f1cc7f0fd14389eb9884ee9f122
Angela <angelakulola4@gmail.com>
2023-06-11 10:10:25 +0300

    Added login

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ import Footer from './components/footer/Footer';
 import MovieList from './components/movieList/MovieList'
 import MovieDetail from './components/movieDetails/MovieDetails'
 import MovieSearch from './components/search/Search'
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <Router>
      <Header />
-
-      
-        <Routes>
-          <Route path="/" element={<MovieList />} />
-          <Route path="/movie" element={<MovieSearch />} />
-          <Route path="/movies/:id" element={<MovieDetail />} />
-        </Routes>
-
-      <Footer />
+     <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/movieList" element={<MovieList />} />
+        <Route path="/movie" element={<MovieSearch />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+      </Routes>
+      <Footer/>
     </Router>
   );
 }
