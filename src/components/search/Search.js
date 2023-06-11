932b7e06de8f36375196b9824b8ccb59066c9986
Ruth <osukukuruth@gmail.com>
2023-06-11 08:12:21 -0400

    Done Assignment

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -5,3 +5,7 @@ function MovieSearch() {
     const [searchQuery, setSearchQuery] = useState('');
     const [searchResults, setSearchResults] = useState([]);
     const apiKey = '2801d62ffaec700bd700b8cdb258437b';
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        try {
