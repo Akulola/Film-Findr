704870bb6836ee51c850a3ef7e265e310eee391d
Ruth <osukukuruth@gmail.com>
2023-06-11 08:10:57 -0400

    Done Assignment

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,3 +1,7 @@
 import React, { useState } from 'react';
 import './movieSearch.css';
 
+function MovieSearch() {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const apiKey = '2801d62ffaec700bd700b8cdb258437b';
