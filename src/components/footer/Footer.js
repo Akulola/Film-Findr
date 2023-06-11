55bc7b0b09f766d760dda063ad05cb80ff4f5e69
reaganmunene <reagannmunene@gmail.com>
2023-06-11 21:13:15 +0300

    updated footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,41 +1,40 @@
 import React, { useState } from 'react';
-import './footer.css'
-
+import './footer.css';
 
 function Footer() {
-    const [info, setInfo] = useState('');
+  const [info, setInfo] = useState('');
 
-    const handleLinkClick = (text) => {
-        setInfo(text);
-    };
+  const handleLinkClick = (text) => {
+    setInfo(text);
+  };
 
-    return (
-        <footer>
-            <div className='footer-content'>
-             <p>&copy; 2023 Film Findr. All rights reserved.</p> 
-                <nav>
-                    <ul className="footer-links">
-                        <li>
-                            <button onClick={() => handleLinkClick('Terms of Service')}>
-                                Terms of Service
-                            </button>
-                        </li>
-                        <li>
-                            <button onClick={() => handleLinkClick('Privacy Policy')}>
-                                Privacy Policy
-                            </button> 
-                        </li>
-                        <li>
-                            <button onClick={() => handleLinkClick('Contact Us')}>
-                                Contact Us
-                            </button>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            {info && <div className="info">{info}</div>}
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2023 Film Findr. All rights reserved.</p>
+        <nav>
+          <ul className="footer-links">
+            <li>
+              <button onClick={() => handleLinkClick('Terms of Service')}>
+                Terms of Service
+              </button>
+            </li>
+            <li>
+              <button onClick={() => handleLinkClick('Privacy Policy')}>
+                Privacy Policy
+              </button>
+            </li>
+            <li>
+              <button onClick={() => handleLinkClick('Contact Us')}>
+                Contact Us
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {info && <div className="info">{info}</div>}
+    </footer>
+  );
 }
 
 export default Footer;
