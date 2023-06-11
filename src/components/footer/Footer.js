684201d175e1db46c36602bf63eb50389b1adc26
Angela <angelakulola4@gmail.com>
2023-06-11 19:33:55 +0300

    Edited footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,41 +1,79 @@
 import React, { useState } from 'react';
-import './footer.css'
-
+import './footer.css';
 
 function Footer() {
-    const [info, setInfo] = useState('');
+  const [info, setInfo] = useState('');
 
-    const handleLinkClick = (text) => {
-        setInfo(text);
-    };
+  const handleLinkClick = (text) => {
+    setInfo(text);
+  };
 
-    return (
-        <footer>
-            <div className='footer-content'>
-             <p>&copy; 2023 Film Findr. All rights reserved.</p> 
-                <nav>
-                    <ul className="footer-links">
-                        <li>
-                            <button onClick={() => handleLinkClick('Terms of Service')}>
-                                Terms of Service
-                            </button>
-                        </li>
-                        <li>
-                            <button onClick={() => handleLinkClick('Privacy Policy')}>
-                                Privacy Policy
-                            </button> 
-                        </li>
-                        <li>
-                            <button onClick={() => handleLinkClick('Contact Us')}>
-                                Contact Us
-                            </button>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            {info && <div className="info">{info}</div>}
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2023 Film Findr. All rights reserved.</p>
+        <nav>
+          <ul className="footer-links">
+            <li>
+              <button onClick={() => handleLinkClick('Contact Us Developers')}>
+                Contact Us
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {info && (
+        <div className="info">
+          {info.includes('Contact Us Developers') ? (
+            <p>
+            Contact Us Developers:{' '}
+            <a
+              href="https://github.com/Akulola"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Angela Kulola
+            </a>
+            {' '}
+            <a
+              href="https://github.com/Ken-Kabutu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Kenneth Wanguka
+            </a>
+            {' '}
+            <a
+              href="https://github.com/RuthOsukuku"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Ruth Osukuku
+            </a>
+            {' '}
+            <a
+              href="https://github.com/reaganmunene"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Reagan Munene
+            </a>
+            {' '}
+            <a
+              href="https://github.com/Camie34"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Mary Mukami
+            </a>
+          </p>
+          ) : (
+            info
+          )}
+        </div>
+      )}
+    </footer>
+  );
 }
 
 export default Footer;
